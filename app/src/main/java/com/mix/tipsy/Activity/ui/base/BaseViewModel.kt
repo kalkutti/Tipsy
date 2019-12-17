d2bb67daec41996f8add4c20a2cd3d0405fafe0e
kalkutti <kalindu.sembakutti@gmail.com>
2019-12-17 14:53:15 -0500

    Tipsy Commit with Initial ViewModel

---
+++ app/src/main/java/com/mix/tipsy/Activity/ui/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.mix.tipsy.Activity.ui.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.MutableLiveData
+
+class BaseViewModel : ViewModel() {
+    private final MutableLiveData<List<Drinks>> drinks
+
+}
