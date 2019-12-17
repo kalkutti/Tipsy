173353b0321e863a7f76dd5f2abcf44c70554947
kalkutti <kalindu.sembakutti@gmail.com>
2019-12-17 17:59:21 -0500

    Tipsy Commit with AbstractFolder

---
+++ app/src/main/java/com/mix/tipsy/Activity/ui/base/AbstractModels/IRcmp.kt
@@ -0,0 +1,5 @@
+package com.mix.tipsy.activity.ui.base.abstractModels
+
+interface IRcmp{
+
+}
---
+++ app/src/main/java/com/mix/tipsy/Activity/ui/base/Rcmp.kt
@@ -0,0 +1,8 @@
+package com.mix.tipsy.activity.ui.base
+
+import com.mix.tipsy.Activity.ui.base.AbstractModels.IRcmp
+
+class Rcmp : IRcmp {
+    val name: String = ""
+    val desc: String = ""
+}
